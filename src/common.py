d5e9966df1bc83f67b982e97c0ecf0f16c0eeb0a
Windroid Xie <windroid.xie@outlook.com>
2021-02-24 17:37:15 +0800

    Complete the log framework construction.

---
+++ src/common.py
@@ -0,0 +1,21 @@
+import json
+import logging
+import logging.config
+
+LOG_CONF_PATH = ".\\conf\\log.conf"
+
+logging.config.fileConfig(LOG_CONF_PATH)
+logger = logging.getLogger('common')
+
+
+def getConfig(configPath, rwopt):
+    config = ''
+    try:
+        with open(configPath, rwopt, encoding='utf-8') as jsonFile:
+            config = json.load(jsonFile)
+        jsonFile.close()
+    except Exception as e:
+        logger.critical("Loading configuration failed:", e)
+        print(e)
+    logger.info(f"Loading configuration:{configPath} successfully")
+    return config
---
+++ src/test.py
@@ -0,0 +1,10 @@
+from common import *
+
+
+def main():
+    APP_CONF_PATH = ".\\conf\\appconf.json"
+    CONFIG_FILE = getConfig(APP_CONF_PATH, 'r')
+
+
+if __name__ == "__main__":
+    main()
