7e60deae26cfacaaa0f7c76f909507d31adc6a89
Windroid Xie <windroid.xie@outlook.com>
2021-02-26 11:42:30 +0800

    noting

--- src/test.py
+++ src/test.py
@@ -4,9 +4,7 @@ from common import *
 def main():
     APP_CONF_PATH = globalvarHelper().getGlobalVar('APP_CONF_PATH')
     CONFIG_FILE = getConfig(APP_CONF_PATH, 'r')
-    print(CONFIG_FILE)
+    #print(CONFIG_FILE)
    
-   
-
 if __name__ == "__main__":
     main()
