72cb0399d4b39c97d8828ab17cdf6574483b0640
Windroid Xie <windroid.xie@outlook.com>
2021-02-28 02:34:53 +0800

    Auxiliary method for simple text messages.

---
+++ src/sendMsgHelper.py
@@ -0,0 +1,27 @@
+from globalvarHelper import globalvarHelper
+import json
+import logging
+import logging.config
+
+logging.config.fileConfig(globalvarHelper().getGlobalVar('LOG_CONF_PATH'))
+logger = logging.getLogger('sendMsgHelper')
+
+def getSimpleMessageJson(agentid,content,touser=None,toparty=None,totag=None,safe=0,\
+    enable_id_trans=0,enable_duplicate_check=0,duplicate_check_interval=1800):
+    message_dic = {\
+        'touser':touser,\
+        'toparty':toparty,\
+        'totag':totag,\
+        'msgtype':'text',\
+        'agentid':agentid,\
+        'text':{\
+            'content':content\
+        },\
+        'safe':safe,\
+        'enable_id_trans':enable_id_trans,\
+        'enable_duplicate_check':enable_duplicate_check,\
+        'duplicate_check_interval':duplicate_check_interval,\
+    }
+    message_json = json.dumps(message_dic)
+    logger.debug(message_json)
+    return message_json
